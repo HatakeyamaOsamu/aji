c84c710cc5fd9b45edf11c9574b7c78fe3b8d67c
HatakeyamaOsamu <osamu.hatakeyama@solidsurface.co.jp>
2025-06-29 21:17:33 +0900

    Update Keyboard component to use VirtualKeyboard

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
+import { VirtualKeyboard } from './VirtualKeyboard';
 
 export const Keyboard: React.FC = () => {
   return (
     <div className="keyboard-container">
-      <div className="virtual-keyboard">
-        {/* TODO: Implement virtual keyboard */}
-        <p>Virtual keyboard coming soon...</p>
-      </div>
+      <VirtualKeyboard />
       <div className="keyboard-hints">
         <span className="hint">Use keyboard keys Z-M (lower octave) and Q-I (upper octave) to play</span>
       </div>
